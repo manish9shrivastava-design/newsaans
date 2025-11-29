810a813c2644226f7f119e3749f7757cbb888c13
manish9shrivastava-design <manish9shrivastava@gmail.com>
2025-11-29 22:24:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-
-const port = process.env.PORT || 3000;
-
-app.use(express.static('public'));
-app.use(bodyParser.json());
-"scripts": {
-  "start": "node app.js"
+{
+  "name": "newsaans",
+  "version": "1.0.0",
+  "description": "SAANS data capture app",
+  "main": "app.js",
+  "scripts": {
+    "start": "node app.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "body-parser": "^1.20.2"
+  },
+  "engines": {
+    "node": "18.x"
+  }
 }
-
-// keep the rest of your routes here
-
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
